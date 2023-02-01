86a6bc4581cfa2b07252b45cd50ba22e5ae095b0
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2023-02-01 19:39:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function navSlide(){
+    document.getElementById('navigation-slide').style.display="inline";
+}
+
+function navSlidesBack(){
+    document.getElementById('navigation-slide').style.display="none";
+}
